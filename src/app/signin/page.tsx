55cbb5a77bd7c6c1e7987dfbf3d2f32bfe2ce1f5
okoye micheal <okoyeikechukwu60@gmail.com>
2024-03-24 23:49:56 +0100

    still tying to fix userdata

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,32 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-
+import { useState, useCallback } from "react";
+interface MyFormData {
+  username: string;
+  email: string;
+  password: string
+}
 export default function Signin() {
+  const [currentUser, setCurrentUser] = useState({});
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState<MyFormData>({
+    username: "",
+    email: "",
+    password:""
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  };
   const router = useRouter();
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="bg-black flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
             Sign in to your account
@@ -18,6 +35,24 @@ export default function Signin() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="space-y-6">
+            {/*  <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  type="username"
+                  autoComplete="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  required
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div> */}
             <div>
               <label
                 htmlFor="email"
@@ -31,7 +66,8 @@ export default function Signin() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  onChange={(e) => setEmail(e.target.value)}
+                  value={formData.email}
+                  onChange={handleInputChange}
                   required
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
@@ -61,7 +97,8 @@ export default function Signin() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
+                  value={formData.password}
+                  onChange={handleInputChange}
                   required
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
@@ -70,15 +107,16 @@ export default function Signin() {
 
             <div>
               <button
-                onClick={() =>
+                onClick={() => {
                   signIn("credentials", {
-                    email,
-                    password,
+                   formData.email,
+                   formData.password,
                     redirect: true,
                     callbackUrl: "/",
-                  })
-                }
-                disabled={!email || !password}
+                  });
+                   localStorage.setItem("mainUser", JSON.stringify(currentUser));
+                }}
+                disabled={!formData.email || !formData.password}
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign in
